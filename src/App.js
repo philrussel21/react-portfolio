856368ab3abc7068c0b6d69fa2f1f8a345339e45
philrussel21 <phil.antiporda21@gmail.com>
2021-01-22 18:36:33 +1000

    Inits components for the main pages; Home, About, Projects & Contacts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
     <div >
-          The basic empty template
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
