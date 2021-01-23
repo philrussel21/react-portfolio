81fa9bd94aa363f98f1f3bf87f45e68e1342e441
philrussel21 <phil.antiporda21@gmail.com>
2021-01-23 16:57:39 +1000

    Adds structure to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import hi from '../media/hi.png';
+import { Typography, Grid } from '@material-ui/core';
 
 export default function Home() {
+  const useStyles = makeStyles((theme) => ({
+    img: {
+      height: 150,
+      width: 150
+    }
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container>
+      <Grid container direction="column">
+        <Grid item>
+          <div>
+            <img src={hi} alt="waving avatar" className={classes.img} />
+          </div>
+        </Grid>
+        <Grid item>
+          <Typography>
+            Phil Antiporda
+          </Typography>
+        </Grid>
+        <Grid item>
+          Rolling Text Animation
+        </Grid>
+        <Grid item>
+          Turning ideas into reality through Software Development
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
