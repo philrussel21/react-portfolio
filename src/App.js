943fec4d70afdc2e770adda0d930d070d785ab1d
philrussel21 <phil.antiporda21@gmail.com>
2021-01-23 15:21:57 +1000

    Adds NotFound component to handle 404

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Router, Link } from '@reach/router';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
 const App = () => {
   return (
-    <div >
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
-    </div>
+    <Router>
+      <Home path="/" />
+      <About path="/about" />
+      <Projects path="/projects" />
+      <Contact path="/contact" />
+      <NotFound default />
+    </Router>
   );
 };
 
