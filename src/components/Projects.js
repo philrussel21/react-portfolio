8afff7269371dbd73733a9cdd6696c21794c9d17
philrussel21 <phil.antiporda21@gmail.com>
2021-01-27 18:21:49 +1000

    Adds more components in Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,17 @@
-import { Container } from '@material-ui/core';
+import { Container, Grid, Paper } from '@material-ui/core';
 import React from 'react';
 
 export default function Projects() {
   return (
     <Container>
       <h1>Projects Page</h1>
+      <Grid container>
+        <Grid item>
+          <Paper>
+
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
