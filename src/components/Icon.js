7fd59d02311ad9f500947dc638028d0ad48e3079
philrussel21 <phil.antiporda21@gmail.com>
2021-01-28 22:25:25 +1000

    Adds a reusable component for the tech icons

---
+++ src/components/Icon.js
@@ -0,0 +1,10 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+
+export default function Icon({ children }) {
+  return (
+    <Grid item>
+      {children}
+    </Grid>
+  );
+}
