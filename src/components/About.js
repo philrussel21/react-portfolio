991eeb1d7e96a1b1342c41e7105dc8788b1ace34
philrussel21 <phil.antiporda21@gmail.com>
2021-01-28 22:26:15 +1000

    Updates Tech Stack section of About me page with existing tech icons

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,24 @@
 import { Container, Typography, Grid, Card, CardContent } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { ReactComponent as Html } from '../media/tech_icons/html.svg';
+import { ReactComponent as Js } from '../media/tech_icons/js.svg';
+import { ReactComponent as NodeJS } from '../media/tech_icons/node.svg';
+// import { ReactComponent as Ruby } from '../media/tech_icons/ruby.svg';
+// import { ReactComponent as Rails } from '../media/tech_icons/rails.svg';
+
+import postgres from '../media/tech_icons/postgres.svg';
+import ruby from '../media/tech_icons/ruby.svg';
+import rails from '../media/tech_icons/rails.svg';
+import css from '../media/tech_icons/css.svg';
 import React from 'react';
+import Icon from './Icon';
 
 export default function About() {
+  const useStyles = makeStyles({
+  });
+
+  const classes = useStyles();
+
   return (
     <Container>
       <Grid container>
@@ -22,6 +39,32 @@ export default function About() {
                 <Typography variant="h2" align="center">
                   Tech Stack
               </Typography>
+                <Grid container>
+                  <Icon>
+                    <Html />
+                  </Icon>
+                  <Icon>
+                    <img src={css} width="50" height="50" alt="CSS3" />
+                  </Icon>
+                  <Icon>
+                    <Js />
+                  </Icon>
+                  <Icon>
+                    {/* <Ruby /> */}
+                    <img src={ruby} width="50" height="50" alt="Ruby" />
+                  </Icon>
+                  <Icon>
+                    {/* <Rails /> */}
+                    <img src={rails} width="50" height="50" alt="Ruby on Rails" />
+                  </Icon>
+                  <Icon>
+                    <NodeJS />
+                  </Icon>
+                  <Icon>
+                    {/* <Postgres /> */}
+                    <img src={postgres} width="50" height="50" alt="PostgreSQL" />
+                  </Icon>
+                </Grid>
               </CardContent>
             </Card>
           </Grid>
