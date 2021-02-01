4d1ed2fbc64a4ab194e6704d69b5a22c6e48401c
philrussel21 <phil.antiporda21@gmail.com>
2021-02-01 17:23:12 +1000

    Adds social and contact links for mobile and pdf download of resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,15 @@
-import { Card, CardContent, CardMedia, Container, Grid, Typography } from "@material-ui/core";
+import { Card, CardContent, CardMedia, Container, Grid, IconButton, Tooltip, Typography } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import okay from '../media/okay.png';
+
+// Social and Contact Icons
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailIcon from '@material-ui/icons/Mail';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import DescriptionIcon from '@material-ui/icons/Description';
+import resume from '../media/PhilAntiporda_Resume.pdf';
 
 export default function Contact() {
 
@@ -12,6 +20,9 @@ export default function Contact() {
       display: 'flex',
       flexDirection: 'column',
       padding: '1.5em',
+      backgroundColor: '#eee',
+      color: '#000',
+      textAlign: 'center',
       // transforms card format depending on screen size
       [theme.breakpoints.up('sm')]: {
         flexDirection: 'row',
@@ -23,7 +34,11 @@ export default function Contact() {
       margin: 'auto',
       padding: '0.6em',
       borderRadius: '50%',
-      backgroundColor: 'white'
+      backgroundColor: '#7d2020'
+    },
+    img: {
+      width: 150,
+      height: 150
     }
   }));
 
@@ -46,11 +61,60 @@ export default function Contact() {
           <Card className={classes.card}>
             <CardMedia>
               <div className={classes.imgBg}>
-                <img src={okay} width={150} height={150} alt="Thumbs-up Avatar" />
+                <img src={okay} className={classes.img} alt="Thumbs-up Avatar" />
               </div>
             </CardMedia>
             <CardContent>
-              oops
+              <Typography variant='h2'>
+                Phil Antiporda
+              </Typography>
+              <Typography variant='subtitle1' display="block">
+                Junior Full Stack Developer
+              </Typography>
+              <Grid container justify="center">
+
+                <Grid item>
+                  <Tooltip title="LinkedIn" arrow>
+                    <IconButton href="https://www.linkedin.com/in/philantiporda/" target="_blank" aria-label="Linkedin">
+                      <LinkedInIcon fontSize="large" />
+                    </IconButton>
+                  </Tooltip>
+
+                </Grid>
+
+                <Grid item>
+                  <Tooltip title="Email" arrow>
+                    <IconButton href="mailto: phil.antiporda21@gmail.com" target="_blank" aria-label="Email">
+                      <MailIcon fontSize="large" />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+
+                <Grid item>
+                  <Tooltip title="Github" arrow>
+                    <IconButton href="https://github.com/philrussel21" target="_blank" aria-label="Github">
+                      <GitHubIcon fontSize="large" />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+
+                <Grid item>
+                  <Tooltip title="Twitter" arrow>
+                    <IconButton href="https://twitter.com/PeelRasel" target="_blank" aria-label="Twitter">
+                      <TwitterIcon fontSize="large" />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+
+                <Grid item>
+                  <Tooltip title="Download Resume" arrow>
+                    <IconButton href={resume} download="PhilAntiporda_Resume.pdf" aria-label="Resume">
+                      <DescriptionIcon fontSize="large" />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+
+              </Grid>
             </CardContent>
           </Card>
         </Grid>
