033887aae090b8c6be4a4ab2847e494cac6f5dca
philrussel21 <phil.antiporda21@gmail.com>
2021-02-05 17:09:57 +1000

    Adds structure to content of proj div

--- src/components/ProjectDiv.js
+++ src/components/ProjectDiv.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { Grid, Card, CardContent, Zoom, Typography } from '@material-ui/core';
+import { Grid, Card, CardContent, Zoom, Typography, Button, IconButton, Tooltip } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import PublicIcon from '@material-ui/icons/Public';
+import CodeIcon from '@material-ui/icons/Code';
 
 
 export default function ProjectDiv({ children, content, bgDesign }) {
@@ -24,6 +26,13 @@ export default function ProjectDiv({ children, content, bgDesign }) {
         width: 250,
         height: 250
       }
+    },
+    projBtn: {
+      border: '1px solid #fff',
+      borderRadius: 25,
+      '&:hover': {
+        background: '#7d2020'
+      }
     }
   }));
 
@@ -42,11 +51,25 @@ export default function ProjectDiv({ children, content, bgDesign }) {
         {isHovering
           ?
           <CardContent className={classes.cardContHover + " hover-text"}>
-            <div className={classes.cardContTxt}>
-              <Typography variant="subtitle1">
-                {content}
-              </Typography>
-            </div>
+            <Grid container direction="column" justify="center" alignContent="space-between" className={classes.cardContTxt}>
+              <Grid item>
+                <Typography variant="h6" align="center">
+                  {content}
+                </Typography>
+              </Grid>
+              <Grid item container justify="space-around">
+                <Tooltip title="Live App" arrow>
+                  <IconButton color="inherit" className={classes.projBtn}>
+                    <PublicIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Source code" arrow>
+                  <IconButton color="inherit" className={classes.projBtn}>
+                    <CodeIcon />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+            </Grid>
           </CardContent>
           :
           <CardContent className={classes.cardCont}>
