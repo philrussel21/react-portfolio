a7fbdacae4a5b65b0e7877d2f46ee066a00cf25e
philrussel21 <phil.antiporda21@gmail.com>
2021-02-20 12:56:47 +1000

    Refactors About and Icon comps for DRYer code and adds css frameworks to tools

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,10 @@ import React from 'react';
 import Icon from './Icon';
 
 // Tech Stack
-import { ReactComponent as Html } from '../media/tech_icons/html.svg';
-import { ReactComponent as Js } from '../media/tech_icons/js.svg';
-import { ReactComponent as NodeJS } from '../media/tech_icons/node.svg';
-import { ReactComponent as Sass } from '../media/tech_icons/sass.svg';
+import html from '../media/tech_icons/html.svg';
+import js from '../media/tech_icons/js.svg';
+import node from '../media/tech_icons/node.svg';
+import sass from '../media/tech_icons/sass.svg';
 import reactIcon from '../media/tech_icons/react.svg';
 import mongodb from '../media/tech_icons/mongodb.svg';
 import postgres from '../media/tech_icons/postgres.svg';
@@ -17,7 +17,7 @@ import css from '../media/tech_icons/css.svg';
 
 // Tech Tools
 import aws from '../media/tech_tools/aws.svg';
-import { ReactComponent as Git } from '../media/tech_tools/git.svg';
+import git from '../media/tech_tools/git.svg';
 import heroku from '../media/tech_tools/heroku.svg';
 import netlify from '../media/tech_tools/netlify.svg';
 import postman from '../media/tech_tools/postman.svg';
@@ -25,6 +25,9 @@ import terminal from '../media/tech_tools/terminal.svg';
 import trello from '../media/tech_tools/trello.svg';
 import jest from '../media/tech_tools/jest.svg';
 import mocha from '../media/tech_tools/mocha.svg';
+import tailwind from '../media/tech_tools/tailwind.svg'
+import matUI from '../media/tech_tools/matUI.svg'
+import bootstrap from '../media/tech_tools/bootstrap.svg'
 
 
 
@@ -70,41 +73,15 @@ export default function About() {
               </Typography>
 
                 <Grid container spacing={1}>
-                  <Icon>
-                    <Html />
-                  </Icon>
-                  <Icon>
-                    <img src={css} className={classes.iconImg} alt="CSS3" />
-                  </Icon>
-                  <Icon>
-                    <Js />
-                  </Icon>
-                  <Icon>
-                    {/* <Ruby /> */}
-                    <img src={ruby} className={classes.iconImg} alt="Ruby" />
-                  </Icon>
-                  <Icon>
-                    {/* <Rails /> */}
-                    <img src={rails} className={classes.iconImg} alt="Ruby on Rails" />
-                  </Icon>
-                  <Icon>
-                    <NodeJS />
-                  </Icon>
-                  <Icon>
-                    {/* <Mongodb /> */}
-                    <img src={mongodb} className={classes.iconImg} alt="MongoDB" />
-                  </Icon>
-                  <Icon>
-                    {/* <Postgres /> */}
-                    <img src={postgres} className={classes.iconImg} alt="PostgreSQL" />
-                  </Icon>
-                  <Icon>
-                    {/* <ReactIcon /> */}
-                    <img src={reactIcon} className={classes.iconImg} alt="React" />
-                  </Icon>
-                  <Icon>
-                    <Sass />
-                  </Icon>
+                  <Icon img={html} alt="HTML5" />
+                  <Icon img={css} alt="CSS3" />
+                  <Icon img={js} alt="Javacript" />
+                  <Icon img={ruby} alt="Ruby" />
+                  <Icon img={rails} alt="Ruby on Rails" />
+                  <Icon img={node} alt="NodeJS" />
+                  <Icon img={mongodb} alt="MongoDB" />
+                  <Icon img={postgres} alt="PostgreSQL" />
+                  <Icon img={reactIcon} alt="React" />
                 </Grid>
               </CardContent>
             </Card>
@@ -118,33 +95,18 @@ export default function About() {
                   Tech Tools
               </Typography>
                 <Grid container spacing={1}>
-                  <Icon>
-                    <img src={aws} className={classes.iconImg} alt="AWS" />
-                  </Icon>
-                  <Icon>
-                    <Git />
-                  </Icon>
-                  <Icon>
-                    <img src={heroku} className={classes.iconImg} alt="Heroku" />
-                  </Icon>
-                  <Icon>
-                    <img src={netlify} className={classes.iconImg} alt="Netlify" />
-                  </Icon>
-                  <Icon>
-                    <img src={postman} className={classes.iconImg} alt="Postman" />
-                  </Icon>
-                  <Icon>
-                    <img src={terminal} className={classes.iconImg} alt="Terminal" />
-                  </Icon>
-                  <Icon>
-                    <img src={trello} className={classes.iconImg} alt="Trello" />
-                  </Icon>
-                  <Icon>
-                    <img src={jest} className={classes.iconImg} alt="Jest" />
-                  </Icon>
-                  <Icon>
-                    <img src={mocha} className={classes.iconImg} alt="Mocha" />
-                  </Icon>
+                  <Icon img={aws} alt="AWS" />
+                  <Icon img={git} alt="Git Source Control" />
+                  <Icon img={heroku} alt="Heroku" />
+                  <Icon img={netlify} alt="Netlify" />
+                  <Icon img={postman} alt="Postman" />
+                  <Icon img={terminal} alt="Terminal" />
+                  <Icon img={trello} alt="Trello" />
+                  <Icon img={jest} alt="Jest" />
+                  <Icon img={mocha} alt="Mocha" />
+                  <Icon img={tailwind} alt="TailwindCSS" />
+                  <Icon img={matUI} alt="Material UI" />
+                  <Icon img={bootstrap} alt="Bootstrap" />
                 </Grid>
               </CardContent>
             </Card>
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,10 +1,22 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
 
-export default function Icon({ children }) {
+export default function Icon({ img, alt }) {
+
+  const useStyles = makeStyles({
+    iconImg: {
+      width: 50,
+      height: 50
+    }
+  });
+
+  const classes = useStyles();
+
   return (
     <Grid item container xs={3} justify="center">
-      {children}
+      {/* {children} */}
+      <img src={img} className={classes.iconImg} alt={alt} />
     </Grid>
   );
 }
